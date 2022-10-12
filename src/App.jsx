bb59c0ce3613f9d9f6dc57a843c0cd764719bf12
Anna Orwat <orwat.ania@gmail.com>
2022-10-12 21:09:00 +0200

    format app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import { useState } from "react"
+import { useState } from "react";
 import productsData from "./data/products.js";
 import { NavBar } from "./components/NavBar/NavBar.jsx";
 import { ProductsList } from "./components/ProductsList/ProductsList.jsx";
 import "./App.css";
 
 function App() {
-  const {products}= productsData;
-  const [searchValue, setSearchValue] =useState("")
+  const { products } = productsData;
+  const [searchValue, setSearchValue] = useState("");
 
-  const filtredProdusts =products.filter(product=>{
-    product.title.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase())
-  })
+  const filtredProdusts = products.filter((product) => {
+    product.title.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase());
+  });
   console.log(products);
-  return <div>
-    <NavBar searchValue={searchValue} 
-    setSearchValue={setSearchValue}/>
-    <ProductsList products={products}/>
-  </div>;
+  return (
+    <div>
+      <NavBar searchValue={searchValue} setSearchValue={setSearchValue} />
+      <ProductsList products={products} />
+    </div>
+  );
 }
 
 export default App;
